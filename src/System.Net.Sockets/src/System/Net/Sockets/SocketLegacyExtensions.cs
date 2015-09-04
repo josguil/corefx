561a085ddae8ed8a5ce930bb6a1516b0389901e4
Guillermo Rangel <josra@microsoft.com>
2015-09-04 16:10:48 -0700

    Addressing Feedback for changing APM methods to extension methods.
    Changes to some of the names of the files and parameters.

--- src/System.Net.Sockets/src/System/Net/Sockets/SocketLegacyExtensions.cs
+++ src/System.Net.Sockets/src/System/Net/Sockets/SocketLegacyExtensions.cs
@@ -1,47 +1,26 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-//------------------------------------------------------------------------------
-// </copyright>
-//------------------------------------------------------------------------------
-
 namespace System.Net.Sockets
 {
-    using System.Collections;
-    using System.Collections.Generic;
-    using System.ComponentModel;
-    using System.Globalization;
-    using System.IO;
-    using System.Net;
-    using System.Runtime.InteropServices;
-    using System.Threading;
-    using System.Diagnostics;
-    using System.Diagnostics.Contracts;
-    using System.Threading.Tasks;
-    using System.Net.Sockets;
-
-    /// <devdoc>
-    /// <para>The <see cref='Sockets.Socket'/> class implements the Berkeley sockets
-    ///    interface.</para>
-    /// </devdoc>
     public static class SocketLegacyExtensions
     {
-        public static System.IAsyncResult BeginAccept(this System.Net.Sockets.Socket socket, System.AsyncCallback callback, object state)
+        public static IAsyncResult BeginAccept(this Socket thisSocket, AsyncCallback callback, object state)
         {
-            return socket.InternalBeginAccept(callback, state);
+            return thisSocket.InternalBeginAccept(callback, state);
         }
-        public static System.IAsyncResult BeginAccept(this System.Net.Sockets.Socket socket, int receiveSize, System.AsyncCallback callback, object state)
+        public static IAsyncResult BeginAccept(this Socket thisSocket, int receiveSize, AsyncCallback callback, object state)
         {
-            return socket.InternalBeginAccept(receiveSize, callback, state);
+            return thisSocket.InternalBeginAccept(receiveSize, callback, state);
         }
 
-        public static System.Net.Sockets.Socket EndAccept(this System.Net.Sockets.Socket socket, out byte[] buffer, System.IAsyncResult asyncResult)
+        public static Socket EndAccept(this Socket thisSocket, out byte[] buffer, IAsyncResult asyncResult)
         {
-            return socket.InternalEndAccept(out buffer, asyncResult);
+            return thisSocket.InternalEndAccept(out buffer, asyncResult);
         }
-        public static System.Net.Sockets.Socket EndAccept(this System.Net.Sockets.Socket socket, out byte[] buffer, out int bytesTransferred, System.IAsyncResult asyncResult)
+        public static Socket EndAccept(this Socket thisSocket, out byte[] buffer, out int bytesTransferred, IAsyncResult asyncResult)
         {
-            return socket.InternalEndAccept(out buffer, out bytesTransferred, asyncResult);
+            return thisSocket.InternalEndAccept(out buffer, out bytesTransferred, asyncResult);
         }
     }        
 }
